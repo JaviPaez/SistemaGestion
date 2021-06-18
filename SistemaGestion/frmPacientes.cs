06aad1aae25402070555404d946c67ceaee951d0
Javier Páez <javip_230@hotmail.com>
2021-06-17 23:27:27 -0300

    bugs

--- SistemaGestion/frmPacientes.cs
+++ SistemaGestion/frmPacientes.cs
@@ -18,7 +18,7 @@ namespace SistemaGestion
 
         //LOAD
         private void frmPacientes_Load(object sender, EventArgs e)
-        {            
+        {
             dtpFecha.Format = DateTimePickerFormat.Short;
             dtpFecha.MinDate = new DateTime(1900, 1, 1);
             dtpFecha.MaxDate = DateTime.Today;
@@ -38,31 +38,51 @@ namespace SistemaGestion
             var mailPaciente = new PacienteeMail();
             var direccPaciente = new PacienteDireccion();
 
-            try
+            if (respuesta == DialogResult.Yes)
             {
-                if (respuesta == DialogResult.Yes)
+                try
                 {
                     paciente.Dni = Convert.ToInt32(txtDni.Text);
                     paciente.Apellido = txtApellido.Text;
                     paciente.Nombre = txtNombre.Text;
                     paciente.FechaNac = dtpFecha.Value;
-                    paciente.ObraSocial = cboObraSocial.SelectedItem.ToString();
-                    paciente.NroAfiliado = Convert.ToInt32(txtNroAfiliado.Text);
+                    if (cboObraSocial.Text == "SELECCIONE" || cboObraSocial.SelectedItem == null)
+                    {
+                        paciente.ObraSocial = "";
+                    }
+                    else paciente.ObraSocial = cboObraSocial.SelectedItem.ToString();
+
+                    if (Int32.TryParse(txtNroAfiliado.Text, out Int32 nroAfiliado))
+                    {
+                        paciente.NroAfiliado = nroAfiliado;
+                    }
 
                     //Telefono
                     telPaciente.Dni = Convert.ToInt32(txtDni.Text);
-                    telPaciente.NroTelefono = Convert.ToInt64(txtTelefono.Text);
+                    if (Int64.TryParse(txtTelefono.Text, out Int64 telefono))
+                    {
+                        telPaciente.NroTelefono = telefono;
+                    }
 
                     //Mail
                     mailPaciente.Dni = Convert.ToInt32(txtDni.Text);
                     mailPaciente.eMail = txtMail.Text;
 
                     //Direccion
-                    direccPaciente.Dni= Convert.ToInt32(txtDni.Text);
-                    direccPaciente.Provincia = cboProvincia.SelectedItem.ToString();
+                    direccPaciente.Dni = Convert.ToInt32(txtDni.Text);
+                    if (cboProvincia.Text != "SELECCIONE" || cboProvincia.SelectedItem == null)
+                    {
+                        direccPaciente.Provincia = "";
+                    }
+                    else direccPaciente.Provincia = cboProvincia.SelectedItem.ToString();
+
+
                     direccPaciente.Localidad = txtLocalidad.Text;
                     direccPaciente.Calle = txtCalle.Text;
-                    direccPaciente.Nro = Convert.ToInt32(txtNro.Text);
+                    if (Int32.TryParse(txtNro.Text, out Int32 nro))
+                    {
+                        direccPaciente.Nro = nro;
+                    }
                     direccPaciente.Piso = txtPiso.Text;
                     direccPaciente.Dpto = txtDpto.Text;
                     direccPaciente.Manzana = txtManzana.Text;
@@ -71,22 +91,22 @@ namespace SistemaGestion
 
                     //Grabo
                     var pacienteMetodo = new PacienteMetodos();
-                    
+
                     Boolean grabo = pacienteMetodo.GrabarPaciente(paciente);
                     pacienteMetodo.GrabarTelefonoPaciente(telPaciente);
                     pacienteMetodo.GrabarMailPaciente(mailPaciente);
                     pacienteMetodo.GrabarDireccionPaciente(direccPaciente);
 
-                    if (grabo == false) MessageBox.Show("Error en grabación", "Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    if (grabo == false) MessageBox.Show("Error en grabación", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     else MessageBox.Show("Grabación correcta", "Grabar", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-            }     
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error en grabación: " + ex.Message,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error en grabación: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
 
-            //ArmarGrilla();
+            MostrarPacienteActual(Convert.ToInt32(txtDni.Text));
         }
 
         //BOTON MODIFICAR
@@ -100,20 +120,31 @@ namespace SistemaGestion
             var mailPaciente = new PacienteeMail();
             var direccPaciente = new PacienteDireccion();
 
-            try
+            if (respuesta == DialogResult.Yes)
             {
-                if (respuesta == DialogResult.Yes)
+                try
                 {
                     paciente.Dni = Convert.ToInt32(txtDni.Text);
                     paciente.Apellido = txtApellido.Text;
                     paciente.Nombre = txtNombre.Text;
                     paciente.FechaNac = dtpFecha.Value;
-                    paciente.ObraSocial = cboObraSocial.SelectedItem.ToString();
-                    paciente.NroAfiliado = Convert.ToInt32(txtNroAfiliado.Text);
+                    if (cboObraSocial.Text == "SELECCIONE" || cboObraSocial.SelectedItem == null)
+                    {
+                        paciente.ObraSocial = "";
+                    }
+                    else paciente.ObraSocial = cboObraSocial.SelectedItem.ToString();
+
+                    if (Int32.TryParse(txtNroAfiliado.Text, out Int32 nroAfiliado))
+                    {
+                        paciente.NroAfiliado = nroAfiliado;
+                    }
 
                     //Telefono
                     telPaciente.Dni = Convert.ToInt32(txtDni.Text);
-                    telPaciente.NroTelefono = Convert.ToInt64(txtTelefono.Text);
+                    if (Int64.TryParse(txtTelefono.Text, out Int64 telefono))
+                    {
+                        telPaciente.NroTelefono = telefono;
+                    }
 
                     //Mail
                     mailPaciente.Dni = Convert.ToInt32(txtDni.Text);
@@ -121,10 +152,19 @@ namespace SistemaGestion
 
                     //Direccion
                     direccPaciente.Dni = Convert.ToInt32(txtDni.Text);
-                    direccPaciente.Provincia = cboProvincia.SelectedItem.ToString();
+                    if (cboProvincia.Text == "SELECCIONE" || cboProvincia.SelectedItem == null)
+                    {
+                        direccPaciente.Provincia = "";
+                    }
+                    else direccPaciente.Provincia = cboProvincia.SelectedItem.ToString();
+
+
                     direccPaciente.Localidad = txtLocalidad.Text;
                     direccPaciente.Calle = txtCalle.Text;
-                    direccPaciente.Nro = Convert.ToInt32(txtNro.Text);
+                    if (Int32.TryParse(txtNro.Text, out Int32 nro))
+                    {
+                        direccPaciente.Nro = nro;
+                    }
                     direccPaciente.Piso = txtPiso.Text;
                     direccPaciente.Dpto = txtDpto.Text;
                     direccPaciente.Manzana = txtManzana.Text;
@@ -140,13 +180,14 @@ namespace SistemaGestion
                     if (modifico == false) MessageBox.Show("Error en modificación", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     else MessageBox.Show("Modificación correcta", "Modificar", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error en modificación: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error en modificación: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
 
-            //ArmarGrilla();
+            MostrarPacienteActual(Convert.ToInt32(txtDni.Text));
         }
 
         //BOTON NUEVO
@@ -172,8 +213,7 @@ namespace SistemaGestion
                     var dt = new DataTable();
                     var al = new PacienteMetodos();
 
-                    int dni;
-                    if (!int.TryParse(txtBuscar.Text, out dni))
+                    if (!int.TryParse(txtBuscar.Text, out int dni))
                     {
                         dt = al.BuscarPacienteApeNom(txtBuscar.Text);
                     }
@@ -201,8 +241,9 @@ namespace SistemaGestion
                 txtApellido.Text = dgvGrilla.CurrentRow.Cells[1].Value.ToString();
                 txtNombre.Text = dgvGrilla.CurrentRow.Cells[2].Value.ToString();
                 dtpFecha.Value = Convert.ToDateTime(dgvGrilla.CurrentRow.Cells[3].Value);
-                cboObraSocial.SelectedItem = dgvGrilla.CurrentRow.Cells[4].Value.ToString();
-                txtNroAfiliado.Text= dgvGrilla.CurrentRow.Cells[5].Value.ToString();
+                //cboObraSocial.SelectedItem = dgvGrilla.CurrentRow.Cells[4].Value.ToString();
+                cboObraSocial.Text = dgvGrilla.CurrentRow.Cells[4].Value.ToString();
+                txtNroAfiliado.Text = dgvGrilla.CurrentRow.Cells[5].Value.ToString();
 
                 txtTelefono.Text = dgvGrilla.CurrentRow.Cells[6].Value.ToString();
 
@@ -237,6 +278,23 @@ namespace SistemaGestion
             txtBuscar.Clear();
         }
 
+        private void MostrarPacienteActual(int dni)
+        {
+            try
+            {
+                var ds = new DataSet();
+                var dt = new DataTable();
+                var al = new PacienteMetodos();
+                
+                dt = al.BuscarPacienteDni(dni);
+
+                if (dt.Rows.Count != 0) dgvGrilla.DataSource = dt;
+            }
+            catch
+            {
+            }
+        }
+
         //Reiniciar campos
         private void ReiniciarCampos()
         {
@@ -245,16 +303,16 @@ namespace SistemaGestion
             txtNombre.Clear();
             txtNroAfiliado.Clear();
             dtpFecha.Value = DateTime.Today;
-            cboObraSocial.Text = "Seleccione";
+            cboObraSocial.Text = "SELECCIONE";
 
-            txtTelefono.Text = "0";
+            txtTelefono.Clear();
 
             txtMail.Clear();
 
-            cboProvincia.Text = "Seleccione";
+            cboProvincia.Text = "SELECCIONE";
             txtLocalidad.Clear();
             txtCalle.Clear();
-            txtNro.Text = "0";
+            txtNro.Clear();
             txtPiso.Clear();
             txtDpto.Clear();
             txtManzana.Clear();
